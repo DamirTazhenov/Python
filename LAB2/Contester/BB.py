448d6693187044be0455df35a873902274d86849
DamirTazhenov <d_tazhenov@kbtu.kz>
2022-02-11 18:15:18 +0600

    F done, BB deleted

--- LAB2/Contester/BB.py
+++
@@ -1,4 +0,0 @@
-n = int (input())
-inp = list(map(int,input().split()))
-inp.sort()
-print(inp[-1]*inp[-2])
--- LAB2/Contester/F.py
+++ LAB2/Contester/F.py
@@ -3,20 +3,26 @@ n= int(input())
 dicti = {}
 rdicti = {}
 values = []
+sorted_names = []
 for i in range(n):
     tmp_1,tmp_2 = input().split()
     if tmp_1 in dicti: # if the same key is existed we just adding money to the balance else creating new record key:value
         tmp_2_2 = int(dicti.get(tmp_1)) + int(tmp_2)#getting existing money and sum new value
-        dicti.update({tmp_1:tmp_2_2})
+        dicti[tmp_1] = tmp_2_2
     else:
-        dicti.update({tmp_1:tmp_2})
-#print (sorted(dicti))
+        dicti.update({tmp_1:int(tmp_2)})
+#print (dicti)
+sorted_names = sorted(dicti)
+max = 0
 for x, y in dicti.items():
     rdicti.update({y:x})
-list_rdicti = sorted(rdicti)
-max = int(list_rdicti[-1])
-for i in list_rdicti:
-    if max!=int(i):
-        print(rdicti.get(i),"has to recieve", max - int(i),"tenge")
+    tmp_int_y = int(y)
+    values.append(int(y))
+    if max<tmp_int_y:
+        max = tmp_int_y
+
+for i in sorted_names:
+    if max!=int(dicti.get(i)):
+        print(i,"has to receive", max - int(dicti.get(i)),"tenge")
     else:
-        print(rdicti.get(i),"is lucky!")
+        print(i,"is lucky!")
