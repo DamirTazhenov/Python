36645aa53e15f982140b82ed5c1e892edc40f944
DamirTazhenov <d_tazhenov@kbtu.kz>
2022-02-17 17:27:50 +0600

    nothing changed

--- LAB2/Contester/AA.py
+++ LAB2/Contester/AA.py
@@ -1,12 +1,10 @@
+def move(integer):
+    global inp
+    for i in range(inp[integer]):
+        
+
 inp = list(map(int,input().split()))
 length = len(inp)
-i,index = 0,0
-max = inp[i]
-while(index!=length):
-    for i in range(index,inp[index]):
-        if inp[i]>max:
-            max = inp[i]
-            index = i
-    print(0)
-    break
+move(inp[1])
+
     
