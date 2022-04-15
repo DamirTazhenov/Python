962558814da81035253068aca3af1a83f5095e85
DamirTazhenov <d_tazhenov@kbtu.kz>
2022-04-15 23:11:22 +0600

    paint modify

--- LAB8-9/paint/paint.py
+++ LAB8-9/paint/paint.py
@@ -1,68 +1,114 @@
-from turtle import position
+import random
 import pygame
+import math
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((640, 480))
+BLACK = (0, 0, 0)
+pygame.init()
 
-    baseLayer = pygame.Surface((640, 480))
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Paint')
+screen.fill(BLACK)
+base = pygame.Surface((800, 600))
 
-    clock = pygame.time.Clock()
-    
-    prevX = -1
-    prevY = -1
-    currentX = -1
-    currentY = -1
-        
-    screen.fill((0, 0, 0))
+draw_on = False
 
-    isMouseDown = False
+pos_last = (0, 0)
 
-    while True:
-        
-        pressed = pygame.key.get_pressed()
+colors = {
+    'red' : (255, 0, 0),
+    'blue' : (0, 0, 255),
+    'green' : (0, 255, 0),
+    'white' : (255, 255, 255),
+    'purple' : (255, 0, 255),
+    'brown' : (150, 75, 0),
+    'olive' : (128, 128, 0),
+    'sky blue' : (0, 255, 255)
+}
 
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+color = colors['green']
+figure = 'pen'
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1: 
-                    isMouseDown = True
-                    currentX =  event.pos[0]
-                    currentY =  event.pos[1]    
-                    prevX =  event.pos[0]
-                    prevY =  event.pos[1]
-
-            if event.type == pygame.MOUSEBUTTONUP:
-                isMouseDown = False
-                baseLayer.blit(screen, (0, 0))
-
-
-            if event.type == pygame.MOUSEMOTION:
-                if isMouseDown:
-                    currentX =  event.pos[0]
-                    currentY =  event.pos[1]
-        
-
-        if isMouseDown and prevX != -1 and prevY != -1 and currentX != -1 and currentY != -1:
-             screen.blit(baseLayer, (0, 0))
-             r = calculateRect(prevX, prevY, currentX, currentY)
-             pygame.draw.rect(screen, (255,255, 255),pygame.Rect(r), 1)
-             #print("{} {} {} {}".format(prevX, prevY, currentX, currentY))
-        
-        pygame.display.flip()
-        clock.tick(60)
-        
-def calculateRect(x1, y1, x2, y2):
+def Rect_pos(x1, y1, x2, y2):
     return pygame.Rect(min(x1, x2), min(y1, y2), abs(x1 - x2), abs(y1 - y2))
-def calculateSquare(x1, y1, x2, y2):
-    x = abs(x1-x2)
-    y = abs(y1,y2)
-    a = min(x,y)
-    return pygame.Rect(x1,y1)
 
-#20,50 400,100
 
-main()
+
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN: # for changing
+            if event.key == pygame.K_b:
+                color = colors['blue']
+            if event.key == pygame.K_g:
+                color = colors['green']
+            if event.key == pygame.K_r:
+                color = colors['red']
+            if event.key == pygame.K_w:
+                color = colors['white']
+            if event.key == pygame.K_o:
+                color = colors['olive']
+            if event.key == pygame.K_n:
+                color = colors['brown']
+            if event.key == pygame.K_u:
+                color = colors['purple']
+            if event.key == pygame.K_a:
+                color = colors['sky blue']
+            if event.key == pygame.K_p:
+                figure = 'pen'
+            if event.key == pygame.K_c:
+                figure = 'circle'
+            if event.key == pygame.K_k:
+                figure = 'rectangle'
+            if event.key == pygame.K_e:
+                figure = 'erase'
+            if event.key == pygame.K_s:
+                figure = 'square'
+            if event.key == pygame.K_t:
+                figure = 'right triangle'
+            if event.key == pygame.K_q:
+                figure = 'equilateral triangle'
+            if event.key == pygame.K_h:
+                figure = 'rhombus'
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            draw_on = True
+            pos_last = event.pos
+        if event.type == pygame.MOUSEBUTTONUP:
+            draw_on = False
+            base.blit(screen, (0, 0))
+        if event.type == pygame.MOUSEMOTION: # functions for figures
+            if draw_on == True:
+                if figure == 'circle':
+                    screen.blit(base, (0, 0))
+                    pygame.draw.circle(screen, color, (pos_last[0], pos_last[1]), int(math.sqrt((event.pos[0] - pos_last[0]) ** 2 + (event.pos[1] - pos_last[1]) ** 2)))
+                if figure == 'rectangle':
+                    screen.blit(base, (0, 0))
+                    a = Rect_pos(pos_last[0], pos_last[1], event.pos[0], event.pos[1])
+                    pygame.draw.rect(screen, color, pygame.Rect(a))
+                if figure == 'pen':
+                    pygame.draw.line(screen, color, [pos_last[0], pos_last[1]], [event.pos[0], event.pos[1]], 5)
+                    pos_last = event.pos
+                if figure == 'erase':
+                    pygame.draw.circle(screen, BLACK, (event.pos[0], event.pos[1]), 9)
+                if figure == 'square':
+                    screen.blit(base, (0, 0))
+                    a = Rect_pos(pos_last[1], pos_last[1], event.pos[1], event.pos[1])
+                    pygame.draw.rect(screen, color, pygame.Rect(a))
+                if figure == 'right triangle':
+                    screen.blit(base, (0, 0))
+                    pygame.draw.polygon(screen, color, [(pos_last[0], pos_last[1] + event.pos[0]), (pos_last[0], pos_last[1]), (pos_last[0] + 2 * event.pos[0], pos_last[1])])
+                if figure == 'equilateral triangle':
+                    screen.blit(base, (0, 0))
+                    pygame.draw.polygon(screen, color, [(pos_last[0] - event.pos[0], pos_last[0] + event.pos[0]), (pos_last[0] + event.pos[0], pos_last[0] + event.pos[0]), (pos_last[0], pos_last[0] - event.pos[0])])
+                if figure == 'rhombus':
+                    pos1 = (pos_last[0] / 2, pos_last[1] / 2 - event.pos[0] / 2)
+                    pos2 = (pos1[0] - event.pos[1] / 2, pos1[1] + event.pos[0] / 2)
+                    pos3 = (pos1[0], pos1[1] + event.pos[0])
+                    pos4 = (pos2[0] + event.pos[1], pos2[1])
+                    points = [pos1, pos2, pos3, pos4]
+                    screen.blit(base, (0, 0))
+                    pygame.draw.polygon(screen, color, points)
+    pygame.display.flip()
+pygame.quit()
